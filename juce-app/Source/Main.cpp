9289263188fc69f040e8e4e9c0c5c51f9b6940c2
Vincent Berthiaume <vberthiaume@gmail.com>
2026-03-11 10:20:36 -0400

    attempt to fix windows printing and dll dependency

--- juce-app/Source/Main.cpp
+++ juce-app/Source/Main.cpp
@@ -1,8 +1,23 @@
 #include <juce_core/juce_core.h>
 #include "rawdger/Core.h"
+#include <iostream>
+
+struct StdOutLogger : public juce::Logger
+{
+    void logMessage (const juce::String& message) override
+    {
+        std::cout << message << std::endl;
+    }
+};
 
 int main()
 {
+    StdOutLogger logger;
+    juce::Logger::setCurrentLogger (&logger);
+
     juce::Logger::writeToLog ("JUCE app says: " + juce::String (rawdger::getString()));
+    DBG("now with DBG JUCE app says: " + juce::String(rawdger::getString()));
+
+    juce::Logger::setCurrentLogger (nullptr);
     return 0;
 }
