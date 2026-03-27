117c4c6b4322878523c9f1cc1329275f2e83815a
Vincent Berthiaume <vberthiaume@gmail.com>
2026-03-27 17:14:54 -0400

    comments

--- qt-app/src/QtMain.cpp
+++ qt-app/src/QtMain.cpp
@@ -3,6 +3,7 @@
 #include <QVBoxLayout>
 #include <QWidget>
 #include <QDebug>
+
 #include "rawdger/Core.h"
 
 int main (int argc, char* argv[])
@@ -13,14 +14,20 @@ int main (int argc, char* argv[])
     window.setWindowTitle ("Rawdger Qt");
     window.resize (300, 200);
 
+    // this looks like it might create a memory leak, but the parent passed in a QObject constructor
+    // adds that object as a child in its object tree and takes ownership of it
     auto* layout = new QVBoxLayout (&window);
+
     auto* recordButton = new QPushButton ("Record");
     recordButton->setCheckable (true);
+    // similarly, adding a widget to a layout also adds it to its object tree
     layout->addWidget (recordButton);
 
+    // connect recordButton's toggled signal to a callback (can be a lambda, a function, or a QObject slot)
     QObject::connect (recordButton, &QPushButton::toggled, [] (bool checked)
     {
-        qDebug() << (checked ? "on" : "off");
+        // qDebug() is Qt's logging function, similar to std::cerr << "yada";
+        qDebug() << rawdger::getString() << (checked ?  "on" : "off");
     });
 
     window.show();
